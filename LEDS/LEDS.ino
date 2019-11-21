9c13312ea9b29f39586b325b3600721b49188d78
Cole Sussmeier <colesussmeier@gmail.com>
2019-11-20 19:59:58 -0500

    fix comments

--- LEDS/LEDS.ino
+++ LEDS/LEDS.ino
@@ -9,7 +9,7 @@
 CRGB leds[NUM_LEDS];
 
 
-int mode = 1; //changed 
+int mode = 1;
 float thishue = 0;
 uint8_t fadeUp = 0;
 int colorDelta = 0;
@@ -80,28 +80,17 @@ if(mode == 4) {
     
   //go foward
   for(int i = 0; i < NUM_LEDS; i++) {
-    // Set the i'th led to red 
     leds[i] = CHSV(hue++, 255, 255);
-    // Show the leds
     FastLED.show(); 
-    // now that we've shown the leds, reset the i'th led to black
-    // leds[i] = CRGB::Black;
     fadeall();
-    // Wait a little bit before we loop around and do it again
     delay(10);
   }
 
-
   //go back  
   for(int i = (NUM_LEDS)-1; i >= 0; i--) {
-    // Set the i'th led to red 
     leds[i] = CHSV(hue++, 255, 255);
-    // Show the leds
     FastLED.show();
-    // now that we've shown the leds, reset the i'th led to black
-    // leds[i] = CRGB::Black;
     fadeall();
-    // Wait a little bit before we loop around and do it again
     delay(10);
   }
 }
